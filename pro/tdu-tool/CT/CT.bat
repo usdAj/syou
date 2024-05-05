97b66c03f7d914f202012db562c0bbf92127134b
taittinger <miya@mail.dendai.ac.jp>
2024-05-05 11:32:15 +0900

    Delete pro/tdu-tool directory

--- pro/tdu-tool/CT/CT.bat
+++
@@ -1,8 +0,0 @@
-echo off
-java CT
-echo errorlevel = %errorlevel%
-if %errorlevel% == 1 (
-    echo 「CT.class」ファイルはありますか
-    echo CLASSPATH の先頭が「.」で開始していますか
-    echo ※環境変数の編集後は，コマンドプロンプトを閉じて，開き直してください
-)
